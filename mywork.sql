38a33e6dd20aaf8b66a0ed09f9677b38089ef3da
Vilas Parab <parab.vilas@gmail.com>
2026-06-18 15:03:21 +0530

    Initial MySQL project Working

---
+++ mywork.sql
@@ -0,0 +1,24 @@
+-- Create Database
+CREATE DATABASE ADay4CollegeDB;
+
+-- Use Database
+USE ADay4CollegeDB;
+
+-- Create Table
+CREATE TABLE Student(
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50),
+    course VARCHAR(30),
+    city VARCHAR(30),
+    marks INT
+);
+
+-- Insert Data
+INSERT INTO Student
+VALUES
+(101,'Amit','BCA','Mumbai',85),
+(102,'Sneha','BSc','Pune',78),
+(103,'Rahul','BCom','Nashik',90);
+
+
+select * from student;
